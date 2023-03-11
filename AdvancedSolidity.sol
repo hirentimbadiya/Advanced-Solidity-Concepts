a58b77a321f08afa661cc6317b8e2e9366e12f0c
Hiren Timbadiya <hirentimbadiya74@gmail.com>
2023-03-11 06:47:13 +0530

    added nested mapping contract

--- AdvancedSolidity.sol
+++ AdvancedSolidity.sol
@@ -37,6 +37,30 @@ contract Mapping {
     }
 }
 
+contract NestedMappings {
+    // Mapping from address => (mapping from uint to bool)
+    mapping(address => mapping(uint => bool)) public nestedMap;
+
+    function get(address _addr1, uint _i) public view returns (bool) {
+        // You can get values from a nested mapping
+        // even when it is not initialized
+        // The default value for a bool type is false
+        return nestedMap[_addr1][_i];
+    }
+
+    function set(
+        address _addr1,
+        uint _i,
+        bool _boo
+    ) public {
+        nestedMap[_addr1][_i] = _boo;
+    }
+
+    function remove(address _addr1, uint _i) public {
+        delete nestedMap[_addr1][_i];
+    }
+}
+
 contract Enum {
     // Enum is the short for Enumerable or Enumerators
     // Human readable types for a set of values
